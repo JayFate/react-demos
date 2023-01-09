db200d6caa3f7aa099b7a85743bdf6cc7dcb8051
JayFate <48240828+JayFate@users.noreply.github.com>
2023-01-09 07:48:57 +0000

    feat: 显示单个文章
    Signed-off-by: JayFate <48240828+JayFate@users.noreply.github.com>

---
+++ demos/redux-essentials-example-app/src/features/posts/SinglePostPage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export const SinglePostPage = ({ match }) => {
+  const { postId } = match.params
+
+  // 每当 useSelector 返回的值为新引用时，组件就会重新渲染
+  // 所以 useSelector 不要返回多余的数据，以免引起组件不必要的重新渲染
+  const post = useSelector(state =>
+    state.posts.find(post => post.id === postId)
+  )
+
+  if (!post) {
+    return (
+      <section>
+        <h2>页面未找到！</h2>
+      </section>
+    )
+  }
+
+  return (
+    <section>
+      <article className="post">
+        <h2>{post.title}</h2>
+        <p className="post-content">{post.content}</p>
+      </article>
+    </section>
+  )
+}
